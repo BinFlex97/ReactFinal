ef32c6a899c9e01fa180b72f739a882a54f25a2f
Hieu <hieukhi12322@gmail.com>
2022-08-30 20:27:15 +0700

    start FilmDetail

--- src/pages/FilmDetail/FilmDetail.jsx
+++ src/pages/FilmDetail/FilmDetail.jsx
@@ -2,56 +2,76 @@ import React, { Fragment } from "react";
 import styleDetail from "./FilmDetail.module.css";
 import { Rate } from "antd";
 import { Progress } from "antd";
-export default function FilmDetail() {
+import HeThongCumRap from "../../Component/HeThongCumRap/HeThongCumRap";
+import { useSelector } from "react-redux";
+export default function FilmDetail(props) {
+  let { data } = useSelector(
+    (rootReducer) => rootReducer.QuanLyHTCRFilmReducer
+  );
+  let { heThongRapChieu } = data;
   return (
-    <div className={styleDetail.bg}>
-      <div className={styleDetail.overlay}>
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <div className="mt-5 d-flex align-items-center justify-content-center">
-                <img src="https://picsum.photos/200/300" alt="" />
-                <div className="text-light ml-5">
-                  <h2 className="text-light">Tên Phim</h2>
-                  <p>Thời gian chiếu</p>
-                  <button className="btn btn-danger">Mua Vé</button>
+    <Fragment>
+      <div
+        style={{
+          backgroundImage: `url(${data.hinhAnh})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          position: "relative",
+          height: "400px",
+        }}
+      >
+        <div className={styleDetail.overlay}>
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <div className="mt-5 d-flex align-items-center justify-content-center">
+                  <img width={250} height={300} src={data.hinhAnh} alt="" />
+                  <div className="text-light ml-5">
+                    <h2 className="text-light">{data.tenPhim}</h2>
+                    <p>{data.ngayKhoiChieu}</p>
+                    <button className="btn btn-danger">Mua Vé</button>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-              }}
-              className="col-6 mt-5"
-            >
-              <div>
-                <Progress
-                  type="circle"
-                  percent={100}
-                  format={() => {
-                    return (
-                      <span
-                        style={{
-                          fontSize: "60px",
-                          color: "white",
-                          fontWeight: "bold",
-                        }}
-                      >
-                        10
-                      </span>
-                    );
-                  }}
-                />
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }}
+                className="col-6 mt-5"
+              >
+                <div>
+                  <Progress
+                    type="circle"
+                    percent={100}
+                    format={() => {
+                      return (
+                        <span
+                          style={{
+                            fontSize: "60px",
+                            color: "white",
+                            fontWeight: "bold",
+                          }}
+                        >
+                          {data.danhGia}
+                        </span>
+                      );
+                    }}
+                  />
+                </div>
+                <Rate allowHalf defaultValue={5} />
+                <p className="text-light mt-3">Số người đánh giá: 15</p>
               </div>
-              <Rate allowHalf defaultValue={5} />
-              <p className="text-light mt-3">Số người đánh giá: 10</p>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="my-5">
+        <HeThongCumRap heThongRapChieu={heThongRapChieu} />
+      </div>
+    </Fragment>
   );
 }
