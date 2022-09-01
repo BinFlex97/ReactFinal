d694e4a9b3798f58d75362fcc5739f62ce900599
Hieu <hieukhi12322@gmail.com>
2022-09-01 22:05:34 +0700

    update filmDetail

--- src/layout/CustomerLayout.jsx
+++ src/layout/CustomerLayout.jsx
@@ -1,45 +1,51 @@
-import React, { Fragment } from 'react'
-import { Route } from 'react-router-dom'
-import { Breadcrumb, Layout} from 'antd';
-import MainHeader from '../Component/MainHeader';
+import React, { Fragment } from "react";
+import { Route } from "react-router-dom";
+import { Breadcrumb, Layout } from "antd";
+import MainHeader from "../Component/MainHeader";
 const { Content, Footer } = Layout;
 
-const CustomerLayout = (props) => { 
-  return <Route exact path={props.path} render={(propsRoute) => {
-    return <Fragment>
-      <Layout className="layout">
-        <MainHeader/>
-        <Content
-          style={{
-            padding: '0 50px',
-          }}
-        >
-          <Breadcrumb
-            style={{
-              margin: '16px 0',
-            }}
-          >
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
+const CustomerLayout = (props) => {
+  return (
+    <Route
+      exact
+      path={props.path}
+      render={(propsRoute) => {
+        return (
+          <Fragment>
+            <Layout className="layout">
+              <MainHeader />
+              <Content
+                style={{
+                  padding: "0 50px",
+                }}
+              >
+                <Breadcrumb
+                  style={{
+                    margin: "16px 0",
+                  }}
+                >
+                  <Breadcrumb.Item>Home</Breadcrumb.Item>
+                  <Breadcrumb.Item>List</Breadcrumb.Item>
+                  <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
 
+                <div className="site-layout-content">
+                  <props.component {...propsRoute} />
+                </div>
+              </Content>
+              <Footer
+                style={{
+                  textAlign: "center",
+                }}
+              >
+                Ant Design ©2018 Created by Ant UED
+              </Footer>
+            </Layout>
+          </Fragment>
+        );
+      }}
+    />
+  );
+};
 
-          <div className="site-layout-content">
-            <props.component/>
-          </div>
-
-        </Content>
-        <Footer
-          style={{
-            textAlign: 'center',
-          }}
-        >
-          Ant Design ©2018 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Fragment>
-  }} />
-}
-
-export default CustomerLayout
+export default CustomerLayout;
--- src/pages/FilmDetail/FilmDetail.jsx
+++ src/pages/FilmDetail/FilmDetail.jsx
@@ -1,76 +1,127 @@
-import React, { Fragment } from "react";
-import styleDetail from "./FilmDetail.module.css";
-import { Rate } from "antd";
-import { Progress } from "antd";
-import HeThongCumRap from "../../Component/HeThongCumRap/HeThongCumRap";
-import { useSelector } from "react-redux";
+import { Progress, Rate } from "antd";
+import React, { Fragment, useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import moment from "moment";
+import ListRapDetailFilm from "./component/ListRapDetailFilm";
+import { layThongTinLichChieuAction } from "../../Redux/Action/QuanLyHeThongRapAction.js";
+const desc = ["terrible", "bad", "normal", "good", "wonderful"];
 export default function FilmDetail(props) {
-  let { data } = useSelector(
-    (rootReducer) => rootReducer.QuanLyHTCRFilmReducer
+  let { id } = props.match.params;
+  let { filmDetail } = useSelector(
+    (rootReducer) => rootReducer.QuanLyPhimReducer
   );
-  let { heThongRapChieu } = data;
+  console.log(filmDetail);
+  let dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(layThongTinLichChieuAction(id));
+  }, []);
+  let { heThongRapChieu } = filmDetail;
+
+  const [value, setValue] = useState(3);
   return (
     <Fragment>
-      <div
-        style={{
-          backgroundImage: `url(${data.hinhAnh})`,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          position: "relative",
-          height: "600px",
-        }}
-      >
-        <div className={styleDetail.overlay}>
-          <div className="container">
-            <div className="row">
-              <div className="col-6">
-                <div className="mt-5 d-flex align-items-center justify-content-center">
-                  <img width={250} height={300} src={data.hinhAnh} alt="" />
-                  <div className="text-light ml-5">
-                    <h2 className="text-light">{data.tenPhim}</h2>
-                    <p>{data.ngayKhoiChieu}</p>
-                    <button className="btn btn-danger">Mua Vé</button>
+      <div>
+        <div
+          style={{
+            backgroundImage: `url(${filmDetail.hinhAnh})`,
+            minHeight: "100vh",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              backgroundColor: "rgba(0,0,0,0.5  )",
+            }}
+          >
+            <div className="container">
+              <div className="row">
+                <div className="col-6" style={{ marginTop: "140px" }}>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <iframe
+                      allowFullScreen="100"
+                      loading="lazy"
+                      width={250}
+                      height={300}
+                      src={filmDetail.trailer}
+                      frameBorder="0"
+                    ></iframe>
+
+                    <div className="ml-5 text-white font-weight-bold">
+                      <p>
+                        {moment(filmDetail.ngayKhoiChieu).format("DD.MM.YYYY")}
+                      </p>
+                      <h4 className="text-warning">{filmDetail.tenPhim}</h4>
+                      <p>
+                        {filmDetail.moTa?.length > 300
+                          ? `${filmDetail.moTa.slice(0, 300)}.....`
+                          : filmDetail.moTa}
+                      </p>
+                      <button className="btn btn-danger">Mua Vé</button>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                }}
-                className="col-6 mt-5"
-              >
-                <div>
-                  <Progress
-                    type="circle"
-                    percent={100}
-                    format={() => {
-                      return (
-                        <span
-                          style={{
-                            fontSize: "60px",
-                            color: "white",
-                            fontWeight: "bold",
-                          }}
-                        >
-                          {data.danhGia}
-                        </span>
-                      );
-                    }}
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    marginTop: "140px",
+                  }}
+                  className="col-6"
+                >
+                  <span className="my-2 text-danger font-weight-bold">
+                    Đánh Giá Phim
+                  </span>
+
+                  <div>
+                    <Progress
+                      type="circle"
+                      percent={100}
+                      format={() => {
+                        return (
+                          <span
+                            style={{
+                              fontSize: "50px",
+                              color: "white",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            {filmDetail.danhGia}đ
+                          </span>
+                        );
+                      }}
+                    />
+                  </div>
+                  <Rate
+                    tooltips={desc}
+                    onChange={setValue}
+                    // value={(`${filmDetail.danhGia}` / 2).toFixed(2)}
+                    value={(`${filmDetail.danhGia}` / 2).toFixed(2)}
                   />
                 </div>
-                <Rate allowHalf defaultValue={5} />
-                <p className="text-light mt-3">Số người đánh giá: 15</p>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="my-5">
-        <HeThongCumRap heThongRapChieu={heThongRapChieu} />
+      <div
+        className="py-5"
+        style={{
+          backgroundImage:
+            "url(https://chiase24.com/wp-content/uploads/2022/02/Tong-hop-cac-hinh-anh-background-dep-nhat-21.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <ListRapDetailFilm heThongRapChieu={heThongRapChieu} />
       </div>
     </Fragment>
   );
